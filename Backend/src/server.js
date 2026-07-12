f1892c1191252a304356e2f3d1e733e61afe9028
Prakhar garg <gargprakhar422@gmail.com>
2026-07-12 21:18:00 +0530

    Final Commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 // Initialize Socket.io
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
